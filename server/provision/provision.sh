0eccc39be5119afa4507fab8db3fd067e92053d2
Julia <julia@JuliadeiMac.local>
2021-10-05 11:54:05 -0400

    add shell script in provision.sh to import data into database

--- server/provision/provision.sh
+++ server/provision/provision.sh
@@ -1,9 +1,11 @@
 sudo apt-get update
 
 # Install Node.js and NPM
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# The certificate for deb.nodesource seems to be expired
+# pass through the certificate error 
+sudo apt install ca-certificates
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
-
 # Install MongoDB
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
@@ -14,3 +16,5 @@ sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
+# import data to db
+mongorestore --drop -d boston311  -c bos311Hex /vagrant/src/db/hexagon.bson
