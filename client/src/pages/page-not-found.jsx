3f52da7cb8a029859c3594875377ae01a3b88a25
Joel Adeniji <57308580+JOOOEG123@users.noreply.github.com>
2022-08-27 21:37:02 -0400

    setup constant for profile

--- client/src/pages/page-not-found.jsx
+++ client/src/pages/page-not-found.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 export function PageNotFound() {
   return (
-    <div className="container">
-      <h2 className="text-center text-warning">Oops! 404</h2>
-      <p className="text-center">
-        The page you are looking for does not exist.
-      </p>
-    </div>
+    <>
+      <div className="col-11 mx-auto pt-5 p-4 shadow ">
+        <h2 className="text-center text-warning fs-1">Oops! 404</h2>
+        <p className="text-center fs-3">
+          The page you are looking for does not exist.
+        </p>
+        <div className="text-end">
+          <a className="btn btn-lg btn-warning text-white" href="/">
+            Go to Homepage
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
