3accc833bf02947f851dc70f26e7ba4206ded89d
JuliaHsu <julia@Juliade-MacBook-Pro.local>
2022-12-20 11:58:43 +0800

    remove config file

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,6 +1,6 @@
-const { readUserPwd } = require("./db_pwd");
-pwd = readUserPwd;
-url_str = "mongodb://readUser:" + pwd + "@34.134.26.155:27017/boston311";
+// const { readUserPwd } = require("./db_pwd");
+// pwd = readUserPwd;
+url_str = "mongodb://readUser:" + "readonly" + "@34.134.26.155:27017/boston311";
 module.exports = {
   url: url_str,
   // url:'mongodb://127.0.0.1/boston311'
