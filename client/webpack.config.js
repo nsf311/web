62f3bf7a796546cca2b9fbcf4fe0211c72298211
JuliaHsu <julia@Juliade-MacBook-Pro.local>
2022-12-21 01:48:42 +0800

    modify webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -56,7 +56,8 @@ module.exports = {
   externals: {
     // global app config object
     config: JSON.stringify({
-      apiUrl: "http://localhost:3000",
+      //apiUrl: "http://localhost:3000",
+      apiUrl:"/api"
     }),
   },
 };
