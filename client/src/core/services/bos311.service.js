1a362e4b86199ba0869977e6c7cf38dea09317ab
Myeong Lee <deeper.myeong.lee@gmail.com>
2022-12-29 11:09:09 -0500

    https test

--- client/src/core/services/bos311.service.js
+++ client/src/core/services/bos311.service.js
@@ -1,23 +1,23 @@
-import http from "../../http-commom";
+import https from "../../http-commom";
 class boston311Service{
     get(id){
-        return http.get(`/hex_data/${id}`);
+        return https.get(`/hex_data/${id}`);
 
     }
     findByHexNum(hexNum){
-        return http.get(`/hex_data/hex_num/${hexNum}`);
+        return https.get(`/hex_data/hex_num/${hexNum}`);
     }
 
     findHexVarByFilter(hexNum, user_type, frequency, reason){
-        return http.get(`/hex_data/hex_vars_filtered/${hexNum}/${user_type}/${frequency}/${reason}`);
+        return https.get(`/hex_data/hex_vars_filtered/${hexNum}/${user_type}/${frequency}/${reason}`);
     }
 
     findRegVarByFilter(user_type, frequency,reason){
-        return http.get(`/hex_data/reg_res_filtered/${user_type}/${frequency}/${reason}`);
+        return https.get(`/hex_data/reg_res_filtered/${user_type}/${frequency}/${reason}`);
     }
 
     findAll(){
-        return http.get(`/hex_data/all_reg_res`);
+        return https.get(`/hex_data/all_reg_res`);
     }
 }
 
